56c9602077a0026fa1acb709c375f354cb1e44a4
juliocesarmfj <111400487+juliocesarmfj@users.noreply.github.com>
2022-09-07 20:40:17 -0300

    Add files via upload

---
+++ questão 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+	int hora,minuto,segundos;
+	printf("escrever a hora ");
+	scanf("%i", &hora);
+	
+	minuto = hora*60;
+	printf("%i minutos ");
+	segundos = minuto*60;
+	printf("%i segundos");
+	
+	
+}
+	
+
+	
