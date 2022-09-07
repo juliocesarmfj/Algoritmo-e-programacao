14ae858008b44e5dd4d92c4f63b14d5137cb0b25
juliocesarmfj <111400487+juliocesarmfj@users.noreply.github.com>
2022-09-07 20:42:07 -0300

    Add files via upload

---
+++ atividade 1/questão 3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+
+int media, valor1, valor2, valor3;
+
+printf ("digite valor 1: "); 
+scanf ("%i", &valor1);
+
+printf ("digite valor 2: ");
+scanf ("%i", &valor2);
+
+printf ("digite valor 3: ");
+scanf ("%i", &valor3);
+
+media= (valor1+valor2+valor3)/3;
+
+printf ("Media: %i", media);
+
+}
---
+++ atividade 1/questão 5.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){;
+int quantirolo, quantimetro, rolo;
+
+printf ("digite quantos metros de rolo ira precisar: ");
+scanf ("%i",quantimetro);
+
+
+
+
+
+
+
+
+}
