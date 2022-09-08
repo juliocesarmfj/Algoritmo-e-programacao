22b323afed853fb2e85e8e9ce38bf00f16f3f256
juliocesarmfj <111400487+juliocesarmfj@users.noreply.github.com>
2022-09-08 13:23:09 -0300

    Update questão 5.cpp

--- atividade 1/questão 5.cpp
+++ atividade 1/questão 5.cpp
@@ -1,15 +1,16 @@
-#include<stdio.h>
-int main(){;
-int quantirolo, quantimetro, rolo;
-
-printf ("digite quantos metros de rolo ira precisar: ");
-scanf ("%i",quantimetro);
-
-
-
-
-
-
-
-
+#include <stdio.h>
+int main (){
+	int metros, rolos,metrosavulsos;
+	
+	printf("digite o valor a: ");
+	scanf ("%i", &metros);
+	
+	rolos=metros / 50;
+	metrosavulsos = metros %50;
+	
+	
+	printf ("rolos: %i\n", rolos);
+	printf ("metros avulsos: %i", metrosavulsos);
+	
+	
 }
