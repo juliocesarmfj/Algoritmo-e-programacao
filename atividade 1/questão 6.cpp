cddbbf75f92208cf774872ce1f446865133fad6d
juliocesarmfj <111400487+juliocesarmfj@users.noreply.github.com>
2022-09-13 21:36:08 -0300

    Update questão 6.cpp

--- atividade 1/questão 6.cpp
+++ atividade 1/questão 6.cpp
@@ -1,10 +1,39 @@
 #include <stdio.h>
 int main (){
-	char nome[50], sexo;
-	int matricula; horastrabalhadas;
-	float valorporhora;salario;
+	float notaA, notaB, media, aprovado, reprovado;
 	
-	printf("digite o nome: ");
-	scanf("%s", nome);
-	printf("digite o sexo: ");
-	scanf ("%c", &sexo); 
+	printf ("peso nota A: 3.5\n");
+    printf ("peso nota B: 7.5\n");
+	
+	printf("digita a nota A: ");
+	scanf ("%f", &notaA);
+	
+	printf("digita a nota B: ");
+	scanf ("%f", &notaB);
+	
+	media= (3.5*notaA+7.5*notaB)/11;
+	
+	if (media>5.5){
+		printf(" _ __   __ _ ___ ___  ___  _   _ \n");
+		printf("| '_ \\ / _` / __/ __|/ _ \\| | | |\n");
+		printf("| |_) | (_| \\__ \\__ \\ (_) | |_| |\n");
+		printf("| .__/ \\__,_|___/___/\\___/ \\__,_|\n");
+		printf("|_| \n");
+		
+	}
+	else
+	if (media<5.5){
+		printf(" _ __ ___ _ __  _ __ _____   _____  _   _ \n");
+		printf("| '__/ _ \\ '_ \\| '__/ _ \\ \\ / / _ \\| | | |\n");
+		printf("| | |  __/ |_) | | | (_) \\ V / (_) | |_| |\n");
+		printf("|_|  \\___| .__/|_|  \\___/ \\_/ \\___/ \\__,_|\n");
+		printf("         |_|                              \n");
+	}
+ 
+	printf("media:%2.f", media);
+	
+	
+	
+	
+	
+}
